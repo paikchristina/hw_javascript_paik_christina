7bab69fc52315e003e54ee9347b0478fec7f2794
Christina Paik <christinapaik@gmail.com>
2017-09-17 15:19:28 -0400

    add 5 president infoas objects in array

--- js/main.js
+++ js/main.js
@@ -1,2 +1,16 @@
-var iceCream = ["Pistachio", "Mint Chocolate Chip", "Cookies and Cream"];
-iceCream[4] = "Cafe au Lait";
+var iceCream = ['Pistachio', 'Mint Chocolate Chip', 'Cookies and Cream'];
+iceCream[4] = 'Cafe au Lait';
+
+var trump = { firstName: 'Donald', lastName: 'Trump', termLength: 'ongoing', party: 'Republican'};
+
+var obama = {firstName: 'Barack', lastName: 'Obama', termLength: '8', party: 'Democratic'};
+
+var wbush = {firstName: 'George W.', lastName: 'Bush', termLength: '8', party: 'Republican'};
+
+var clinton = {firstName: 'Bill', lastName: 'Clinton', termLength: '8', party: 'Democratic'};
+
+var bush = {firstName: 'George H.W.', lastName: 'Bush', termLength: '4', party: 'Republican'};
+
+var presidents = ['trump', 'obama', 'wbush', 'clinton', 'bush'];
+
+console.log (presidents.wbush.firstName + presidents.wbush.lastName);
